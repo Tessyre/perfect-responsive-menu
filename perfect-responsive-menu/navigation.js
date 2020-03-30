00e56838273809716a3864a213e3ef701ee93d01
Tessyre <51118268+Tessyre@users.noreply.github.com>
2020-03-30 14:42:22 +0200

    Add files via upload

---
+++ perfect-responsive-menu/navigation.js
@@ -0,0 +1,9 @@
+const toggleMenu = document.querySelector('.navigation button');
+const menu = document.querySelector('.navigation ul');
+$('body').addClass('stop-scrolling')
+
+toggleMenu.addEventListener('click', function() {
+	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
+	toggleMenu.setAttribute('aria-expanded', !open);
+	menu.hidden = !menu.hidden;
+});
